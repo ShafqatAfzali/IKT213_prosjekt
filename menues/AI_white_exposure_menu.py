5377c6d22eea2e962246927667e74cba48785de3
Sindre C. T. Bryhn <scbryhn@uia.no>
2025-11-09 20:43:19 +0100

    WIP: CHEKPOINT 2

--- menues/AI_white_exposure_menu.py
+++ menues/AI_white_exposure_menu.py
@@ -1,11 +1,12 @@
 from tkinter import Menu
 
 import cv2
-import numpy as np
-
 import torch
-from torchvision import transforms
 from PIL import Image
+
+from Deep_White_Balance.PyTorch.arch import deep_wb_single_task
+from Deep_White_Balance.PyTorch.utilities.deepWB import deep_wb
+import Deep_White_Balance.PyTorch.utilities.utils as utls
 
 from classes.state import State
 from helpers.image_render import update_display_image
@@ -13,7 +14,20 @@ from helpers.image_render import update_display_image
 
 def create_ai_white_exposure_menu(state: State, menu_bar):
     def predict_gains():
-        pass
+        net_awb = deep_wb_single_task.deepWBnet()
+        device = torch.device('cpu')
+        net_awb.to(device=device)
+        net_awb.load_state_dict(torch.load("Deep_White_Balance/PyTorch/models/net_awb.pth", map_location=device))
+        net_awb.eval()
+
+        resized_image = cv2.resize(state.cv_image_full, (656, 656))
+        rgb_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
+        pil_img = Image.fromarray(rgb_image)
+        out_awb = deep_wb(pil_img, task="awb", net_awb=net_awb, device=device, s=656)
+        result_awb = utls.to_image(out_awb)
+        result_awb.save('AAAAAAAAAAAAAAAAAA_AWB.png')
+
+
 
     def set_gains(b,g,r):
         state.adjustment_values["b_gain"] = b
@@ -28,6 +42,5 @@ def create_ai_white_exposure_menu(state: State, menu_bar):
 
 
     ai_white_exposure_menu = Menu(menu_bar, tearoff=0)
-    ai_white_exposure_menu.add_command(label="Test", command=apply_auto)
     ai_white_exposure_menu.add_command(label="AAAHHH", command=predict_gains)
     menu_bar.add_cascade(label="White exposure", menu=ai_white_exposure_menu)
