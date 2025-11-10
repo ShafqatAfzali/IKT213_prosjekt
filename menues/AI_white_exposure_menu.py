8ac38fd697c8c9384adc657380af86f10165c9d3
Sindre Bryhn <scbryhn@uia.no>
2025-11-10 11:32:01 +0100

    FINISHED

--- menues/AI_white_exposure_menu.py
+++ menues/AI_white_exposure_menu.py
@@ -1,6 +1,5 @@
-from tkinter import Menu
-
 import cv2
+import numpy as np
 import torch
 from PIL import Image
 
@@ -13,6 +12,13 @@ from helpers.image_render import update_display_image
 
 
 def create_ai_white_exposure_menu(state: State, menu_bar):
+    def to_linear(img):
+        if not isinstance(img, np.ndarray):
+            img = np.array(img)
+
+        img = img.astype(np.float32) / 255.0
+        return np.power(img, 2.2)
+
     def predict_gains():
         net_awb = deep_wb_single_task.deepWBnet()
         device = torch.device('cpu')
@@ -20,28 +26,35 @@ def create_ai_white_exposure_menu(state: State, menu_bar):
         net_awb.load_state_dict(torch.load("Deep_White_Balance/PyTorch/models/net_awb.pth", map_location=device))
         net_awb.eval()
 
-        resized_image = cv2.resize(state.cv_image_full, (656, 656))
-        rgb_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
+        rgb_image = cv2.cvtColor(state.cv_image_full, cv2.COLOR_BGR2RGB)
         pil_img = Image.fromarray(rgb_image)
-        # TODO: Picture is saved in 656 and maybe to "dull" colors
         out_awb = deep_wb(pil_img, task="awb", net_awb=net_awb, device=device, s=656)
         result_awb = utls.to_image(out_awb)
-        result_awb.save('AAAAAAAAAAAAAAAAAA_AWB.png')
+
+        orig_lin = to_linear(rgb_image)
+        corr_lin = to_linear(result_awb)
+
+        mean_orig = orig_lin.mean(axis=(0, 1))
+        mean_corr = corr_lin.mean(axis=(0, 1))
+
+        rgb_gain = mean_corr / (mean_orig + 1e-8)
+
+        rgb_gain /= rgb_gain.mean()
+        return rgb_gain.tolist()
 
 
-
-    def set_gains(b,g,r):
+    def set_gains(image, b,g,r):
         state.adjustment_values["b_gain"] = b
         state.adjustment_values["g_gain"] = g
         state.adjustment_values["r_gain"] = r
+        return image
 
     def apply_auto():
-        r,g,b = predict_gains()
+        [r,g,b] = predict_gains()
+
         state.operations.append([set_gains, [], {"b": b, "g": g, "r": r}])
         state.redo_stack.clear()
         update_display_image(state)
 
 
-    ai_white_exposure_menu = Menu(menu_bar, tearoff=0)
-    ai_white_exposure_menu.add_command(label="AAAHHH", command=predict_gains)
-    menu_bar.add_cascade(label="White exposure", menu=ai_white_exposure_menu)
+    menu_bar.add_command(label="Auto white balance", command=apply_auto)
